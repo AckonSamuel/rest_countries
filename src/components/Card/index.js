bedf407764a60dfb14262a91afac101681ffa5f3
Ackon Samuel <ackons433@gmail.com>
2023-03-25 15:22:42 +0000

    Define CountryCard component

---
+++ src/components/Card/index.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
+const CountryCard = ({
+    officialName,
+    region,
+    capital,
+    flagsvg,
+    flagsalt,
+    population,
+}) => {
+    const navigate = useNavigate();
+
+    const goTo = (sm) => {
+        navigate(sm);
+    };
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={flagsvg}
+                    alt={flagsalt}
+                    onClick={() => goTo(`/details/${officialName}`)}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {officialName}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Population: {population} <br />
+                        Region: {region} <br />
+                        Capital: {capital}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    )
+};
+
+CountryCard.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    flagsalt: PropTypes.string.isRequired,
+    flagsvg: PropTypes.string.isRequired,
+    region: PropTypes.string.isRequired,
+    population: PropTypes.number.isRequired,
+    capital: PropTypes.string.isRequired,
+};
+
+export default CountryCard;

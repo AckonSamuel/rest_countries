029f8c47311bc71611abc38679b6be461aaaee82
Ackon Samuel <ackons433@gmail.com>
2023-03-25 18:30:15 +0000

    Define search bar component

---
+++ src/components/Searchbar/index.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Searchbar = ({ textListener, searchText }) => {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Country"
+        type="search"
+        value={searchText}
+        inputProps={{ 
+            'aria-label': 'search country',
+            onChange: (e) => textListener(e.target.value)
+     }}
+      />
+    </Paper>
+  );
+};
+
+Searchbar.propTypes = {
+    textListener: PropTypes.func.isRequired,
+    searchText: PropTypes.string.isRequired,
+};
+
+export default Searchbar;

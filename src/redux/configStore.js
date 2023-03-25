67d9c03927da773f18c6ecb275ef34d3218a6ce5
Ackon Samuel <ackons433@gmail.com>
2023-03-25 05:00:19 +0000

    Configure the store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import rootReducer from './slices/getCountries';
 
 const MyMiddlewares = [logger, thunk];
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        rootReducer,
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(MyMiddlewares),
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 

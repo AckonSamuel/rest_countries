b25706e2bf15bb0a00b7a1a0b14601a31cd4d2fe
Ackon Samuel <ackons433@gmail.com>
2023-03-26 22:02:28 +0000

    Defining return button component

---
+++ src/components/Details/Return.jsx
@@ -0,0 +1,13 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const Return = () =>
+    <Box>
+        <Button>
+            <ArrowBackIcon />
+            Back
+        </Button>
+    </Box>;
+
+export default Return;

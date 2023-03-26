6e158aeddb9ab3791da28c3695ed13c3b319a52a
Ackon Samuel <ackons433@gmail.com>
2023-03-26 02:54:18 +0000

    Define navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Brightness3Icon from '@mui/icons-material/Brightness3';
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Where in the world?
+          </Typography>
+          <Box>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Brightness3Icon />
+          </IconButton>
+          <Button color="inherit">Dark Mode</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Navbar;

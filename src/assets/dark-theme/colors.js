d9e56f43d993a73eca1431106cec31fd0f5117e1
Ackon Samuel <ackons433@gmail.com>
2023-05-08 16:22:52 +0000

    Add dark-theme colors

---
+++ src/assets/dark-theme/colors.js
@@ -0,0 +1,28 @@
+const colors = {
+    mode: 'dark',
+
+    background: {
+        default: "#202C36",
+        primary: "#2B3844",
+        secondary: "#fff",
+        tertiary: "#000000",
+        main: "#979797",
+        focus: "#808080",
+    },
+
+    filter: {
+        main: "#2B3844",
+    },
+
+    text: {
+        main:"#fff",
+    },
+
+    shadow: {
+        primary: "0px 0px 7px 2px rgba(0, 0, 0, 0.0294384)",
+        secondary: "0px 2px 4px rgba(0, 0, 0, 0.0562443)",
+    },
+
+};
+
+export default colors;

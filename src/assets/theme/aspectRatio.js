89f41c3313ec2611b211cf3696001b0f332dedd2
Ackon Samuel <ackons433@gmail.com>
2023-05-09 12:54:57 +0000

    Fix linter errors

--- src/assets/theme/aspectRatio.js
+++ src/assets/theme/aspectRatio.js
@@ -1,15 +1,14 @@
 const getAspectRatio = (width, height) => {
-    const gcd = (a, b) => {
-        if ( b === 0) {
-            return a;
-        } else {
-            return gcd(b, a % b);
-        }
-    };
+  const gcd = (a, b) => {
+    if (b === 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  };
 
-    const ratio = gcd( width, height);
+  const ratio = gcd(width, height);
 
-    return `${width / ratio}:${height / ratio}`;
-}
+  return `${width / ratio}:${height / ratio}`;
+};
 
 export default getAspectRatio;

312575153ac7c2e5fe5d963802a976aec7429993
Ackon Samuel <ackons433@gmail.com>
2023-05-12 23:36:54 +0000

    Clean up dependencies

--- src/__tests__/countriesSlice.test.js
+++ src/__tests__/countriesSlice.test.js
@@ -44,84 +44,84 @@ describe('Redux Slice', () => {
       expect(actions[0]).toEqual(expectedAction);
     });
 
-        it('should create an action to search countries by title', () => {
-            const searchQuery = 'Germany';
-            const expectedAction = { type: 'countries/searchTitle', payload: searchQuery };
+    it('should create an action to search countries by title', () => {
+      const searchQuery = 'Germany';
+      const expectedAction = { type: 'countries/searchTitle', payload: searchQuery };
 
-            const action = searchTitle(searchQuery);
-            expect(action).toEqual(expectedAction);
-        });
-
-        it('should create an action to filter countries by region', () => {
-            const region = 'Europe';
-            const expectedAction = { type: 'countries/filterRegion', payload: region };
-
-            const action = filterRegion(region);
-            expect(action).toEqual(expectedAction);
-        });
-
-        it('should create an action to set the search query', () => {
-            const searchQuery = 'search query';
-            const expectedAction = { type: 'countries/search', payload: searchQuery };
-
-            const action = search(searchQuery);
-            expect(action).toEqual(expectedAction);
-        });
-
-        it('should create an action to set the filter', () => {
-            const filterValue = 'filter value';
-            const expectedAction = { type: 'countries/filter', payload: filterValue };
-
-            const action = filter(filterValue);
-            expect(action).toEqual(expectedAction);
-        });
+      const action = searchTitle(searchQuery);
+      expect(action).toEqual(expectedAction);
     });
 
-    describe('Reducer', () => {
-        it('should return the initial state', () => {
-            const initialState = {
-                countries: [],
-                error: '',
-                loading: false,
-                nameSearch: [],
-                regionFilter: [],
-                search: '',
-                filter: '',
-            };
+    it('should create an action to filter countries by region', () => {
+      const region = 'Europe';
+      const expectedAction = { type: 'countries/filterRegion', payload: region };
 
-            const action = { type: 'UNKNOWN_ACTION' };
-            const nextState = reducer(undefined, action);
-
-            expect(nextState).toEqual(initialState);
-        });
-
-        it('should handle countriesFetch.pending action', () => {
-            const action = { type: 'countries/countriesFetch/pending' };
-            const nextState = reducer(undefined, action);
-
-            expect(nextState.loading).toBe(true);
-            expect(nextState.countries).toEqual({});
-            expect(nextState.error).toBe('');
-        });
-
-        it('should handle countriesFetch.fulfilled action', () => {
-            const payload = ['country1', 'country2'];
-            const action = { type: 'countries/countriesFetch/fulfilled', payload };
-            const nextState = reducer(undefined, action);
-
-            expect(nextState.countries).toEqual(payload);
-            expect(nextState.loading).toBe(false);
-            expect(nextState.error).toBe('');
-        });
-
-        it('should handle countriesFetch.rejected action', () => {
-            const error = 'Error message';
-            const action = { type: 'countries/countriesFetch/rejected', error: { message: error } };
-            const nextState = reducer(undefined, action);
-
-            expect(nextState.error).toBe(error);
-            expect(nextState.loading).toBe(false);
-            expect(nextState.countries).toEqual({});
-        });
+      const action = filterRegion(region);
+      expect(action).toEqual(expectedAction);
     });
-})
+
+    it('should create an action to set the search query', () => {
+      const searchQuery = 'search query';
+      const expectedAction = { type: 'countries/search', payload: searchQuery };
+
+      const action = search(searchQuery);
+      expect(action).toEqual(expectedAction);
+    });
+
+    it('should create an action to set the filter', () => {
+      const filterValue = 'filter value';
+      const expectedAction = { type: 'countries/filter', payload: filterValue };
+
+      const action = filter(filterValue);
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('Reducer', () => {
+    it('should return the initial state', () => {
+      const initialState = {
+        countries: [],
+        error: '',
+        loading: false,
+        nameSearch: [],
+        regionFilter: [],
+        search: '',
+        filter: '',
+      };
+
+      const action = { type: 'UNKNOWN_ACTION' };
+      const nextState = reducer(undefined, action);
+
+      expect(nextState).toEqual(initialState);
+    });
+
+    it('should handle countriesFetch.pending action', () => {
+      const action = { type: 'countries/countriesFetch/pending' };
+      const nextState = reducer(undefined, action);
+
+      expect(nextState.loading).toBe(true);
+      expect(nextState.countries).toEqual({});
+      expect(nextState.error).toBe('');
+    });
+
+    it('should handle countriesFetch.fulfilled action', () => {
+      const payload = ['country1', 'country2'];
+      const action = { type: 'countries/countriesFetch/fulfilled', payload };
+      const nextState = reducer(undefined, action);
+
+      expect(nextState.countries).toEqual(payload);
+      expect(nextState.loading).toBe(false);
+      expect(nextState.error).toBe('');
+    });
+
+    it('should handle countriesFetch.rejected action', () => {
+      const error = 'Error message';
+      const action = { type: 'countries/countriesFetch/rejected', error: { message: error } };
+      const nextState = reducer(undefined, action);
+
+      expect(nextState.error).toBe(error);
+      expect(nextState.loading).toBe(false);
+      expect(nextState.countries).toEqual({});
+    });
+  });
+});
